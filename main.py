53b5def33a175eee12019ee0062a82b34eb96136
Anthony Salani <asalani93@gmail.com>
2014-12-03 18:15:22 -0500

    adds dbug mode, more images

--- main.py
+++ main.py
@@ -4,16 +4,42 @@ import cv2
 import maze
 import util
 import graphs
- 
+
+def colorType(s):
+  try:
+    r, g, b = map(int, s.split(','))
+    return (r, g, b)
+  except:
+    raise TypeError
+
+start = None
+finish = None
+
+def mouseCallback(event, x, y, flags, params):
+  global start, finish
+  if event == cv2.EVENT_LBUTTONDOWN and start == None:
+    start = (x, y)
+  elif event == cv2.EVENT_LBUTTONDOWN and finish == None:
+    finish = (x, y)
+
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required = True, help = "Path to the image")
-ap.add_argument("-c", "--clusters", required = True, type = int, help = "# of clusters")
+ap.add_argument('-i', '--image', required = True, help = 'Path to the image')
+ap.add_argument('-r', '--rgb', required = False, type = colorType, default = (0, 0, 255), help = 'Red component of color')
+
+ap.add_argument('-b', '--bins', required = False, type = int, default = 8, help = '# of bins when making graph')
+ap.add_argument('-c', '--clusters', required = False, type = int, default = 2, help = '# of clusters used in color quantization')
+
+ap.add_argument('--debug', dest = 'debug', action = 'store_true')
+ap.add_argument('--no-debug', dest = 'debug', action = 'store_false')
+ap.set_defaults(debug = False)
+
 args = vars(ap.parse_args())
 
 # load the image and grab its width and height
 image = cv2.imread(args["image"])
-backup = image.copy()
+original = image.copy()
+
 #image = util.areaResize(image, 640, 480)
 (h, w) = image.shape[:2]
  
@@ -21,28 +47,42 @@ backup = image.copy()
 image = maze.quantizeColors(image, args['clusters'])
 image = maze.splitQuantized(image)
 image = maze.completeShape(image)
-#image = cv2.bitwise_not(image)
-#image = zhangsuen.computeAB(image)
 image = maze.makeSkeleton(image)
 paths = maze.makePaths(image)
+graph = graphs.Graph(paths, binSize = args['bins'])
 
-graph = graphs.Graph(paths, binSize = 4)
-#print graph.getClosestNode((300, 200))
+r, g, b = args['rgb']
 
-start = (190, 220)
-finish = (635, 380)
-path = graph.findPath(start, finish)
+if args['debug'] == True:
+  for node in graph.neighbors:
+    for neighbor in graph.neighbors[node]:
+      p1 = graph.nodes[node]
+      p2 = graph.nodes[neighbor]
+      cv2.line(original, p1, p2, (b, g, r))
 
-for p1, p2 in util.transitions(path):
-  cv2.line(backup, p1, p2, (255, 0, 0), thickness = 2)
+cv2.namedWindow('Original Maze')
+cv2.imshow('Original Maze', original)
 
+cv2.setMouseCallback('Original Maze', mouseCallback)
 
-'''
-image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-image = cv2.subtract(backup, image)
-'''
+while True:
+  while finish == None:
+    cv2.waitKey(10)
 
-# show the image
-cv2.namedWindow('A')
-cv2.imshow('A', backup)
-cv2.waitKey(0)
+  path = graph.findPath(start, finish)
+  start = None
+  finish = None
+  if path == None:
+    continue
+
+  # draw the line over the image
+  solved = original.copy()
+  for p1, p2 in util.transitions(path):
+    cv2.line(solved, p1, p2, (b, g, r), thickness = 2)
+
+  # show the image
+  cv2.namedWindow('Solved Maze')
+  cv2.imshow('Solved Maze', solved)
+  cv2.waitKey(0)
+  cv2.destroyWindow('Solved Maze')
+
--- util.py
+++ util.py
@@ -4,6 +4,7 @@ import cv2
 import numpy
 import math
 
+# Resizes an image to have the same number of pixels as x * y.
 def areaResize(img, x, y):
   area1 = x * y
 
@@ -17,5 +18,6 @@ def areaResize(img, x, y):
   newImg = cv2.resize(img, (newWidth, newHeight))
   return newImg
 
+# Converts a list of points to a list of pairs of points, denoting transitions between points.
 def transitions(points):
   return zip(points[:-1], points[1:])
